6ef1a9a41b4049863a34d5042013023c1beb9d99
CMcLuskie <CMcLuskie@users.noreply.github.com>
2017-10-25 13:43:33 +0100

    Revert "Code Update 4"
    This reverts commit 5a3c83334f14c37add6932d092f9ea1e08c90511.

---
+++ Happy Hounds VR/Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public List<Vector3> nodes;
+    public List<Vector3> blockedNodes;
+
+    int lengthX = 24;
+    int indoorLengthZ = 8;
+    int outdoorLengthZ = 10;
+
+    private void Start()
+    {
+        InstantiateNodesX();
+        InstantiateNodesZ();
+    } 
+
+    void InstantiateNodesX()
+    {
+        for (int i = 0; i < lengthX; i++)
+            nodes.Add(new Vector3(-19 + i, 0, 4));
+    }
+
+    void InstantiateNodesZ()
+    {
+        for (int i = 0; i < indoorLengthZ; i++)
+            nodes.Add(new Vector3(4, 0, 4 - indoorLengthZ));
+        for (int j = 0; j < outdoorLengthZ; j++)
+            nodes.Add(new Vector3(-19, 0, -4 - outdoorLengthZ));
+    }
+}
