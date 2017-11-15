8ca84e0d953a281953c1ac593f613743c1660fe5
CMcLuskie <CMcLuskie@users.noreply.github.com>
2017-11-15 13:51:13 +0000

    Fix

---
+++ Happy Hounds VR/Assets/Scripts/Dog/DogBrain.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogBrain : Dog {
+
+    [SerializeField]
+    protected GameObject toyPrefab;
+    GameObject newToy;
+
+    [SerializeField]
+    protected Transform mouth;
+    [SerializeField]
+    protected Transform foodBowl;
+    [SerializeField]
+    protected Transform waterBowl;
+
+    float wanderTimer;
+
+    bool pickedUp;
+    float move;
+
+     
+
+    public bool closeToToy;
+    public bool closeToPlayer;
+    public bool toySeen;
+    public bool toyCaught;
+    public bool followPlayer;
+    [HideInInspector]
+    public GameObject toy;
+
+    public enum Seekable { Player, Toy };
+    enum DogBehaviours { FollowToy, Wandering, FollowPlayer, FollowFood};
+
+    #region Unity Methods
+        private void OnEnable()
+        {
+
+            MainHand.HeadScratch += HeadScratch;
+            MainHand.BodyScratch += BodyScratch;
+            MainHand.StopBodyScratch += StopBodyScratch;
+            MainHand.StopHeadScratch += StopHeadScratch;
+            SecondHand.DogSit += DogSit;
+            SecondHand.DogCall += DogCall;
+        }
+
+   
+
+    private void OnDisable()
+        {
+            MainHand.HeadScratch -= HeadScratch;
+            MainHand.BodyScratch -= BodyScratch;
+            MainHand.StopBodyScratch -= StopBodyScratch;
+            MainHand.StopHeadScratch -= StopHeadScratch;
+            SecondHand.DogSit -= DogSit;
+            SecondHand.DogCall -= DogCall;
+    }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+    #endregion
+
+    #region Decisions
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Wandering();
+
+        if (!isLerping)
+        {
+            animator.SetFloat("Move", 0.5f);
+        }
+
+        if (transform.position.y < 0.22f)
+        {
+            Vector3 fix = new Vector3(transform.position.x, 0.22f, transform.position.z);
+            transform.position = fix;
+        }
+
+        if (OutOfBounds())
+        {
+           // ResetPosition();
+        }
+
+        if (toySeen)
+            DecisionMaker(DogBehaviours.FollowToy);
+
+        if (toyCaught)
+            followPlayer = true;
+
+        if (followPlayer)
+            DecisionMaker(DogBehaviours.FollowPlayer);
+
+        if (pickedUp)
+        {
+
+        }
+    }
+
+    void DecisionMaker(DogBehaviours behaviours)
+    {
+      
+        switch (behaviours)
+        {
+            case DogBehaviours.FollowFood:
+                GoToPoint(foodBowl.position, 0.01f);
+
+                break; 
+            case DogBehaviours.FollowPlayer:
+                GoToPoint(PlayerPos(), 0.01f);
+
+                if (toyCaught)
+                {
+                    if (closeToPlayer)
+                        DropToy();
+                    else
+                        newToy.transform.position = mouth.position;
+                }
+                    
+                break;
+            case DogBehaviours.FollowToy:
+                GoToPoint(ToyPos(toy), 0.01f);
+
+                if (toyCaught)
+                {
+                    followPlayer = true;
+                    toySeen = false;
+                }
+
+                break;
+            case DogBehaviours.Wandering:
+                GoToPoint(gridScript.GetRandomNode().coord, 0.01f);
+
+                break;
+        }
+    }
+#endregion
+
+    #region Toy
+    void PickUpToy()
+        {
+            if (!toyCaught)
+            {
+                print("pick up");
+                Destroy(GameObject.FindGameObjectWithTag("Toy"));
+                newToy = Instantiate(toyPrefab, mouth.position, Quaternion.identity);
+            }
+            toyCaught = true;
+            pickedUp = true;
+        }
+
+        void DropToy()
+        {
+            toyCaught = false;
+            //Vector3 newPos = new Vector3();
+            //newPos = new Vector3(-10, .22f, -10);
+            //while (transform.position != newPos)
+            //    GoToPoint(newPos, .01f);
+            transform.LookAt(PlayerPos());
+        }
+
+        public Vector3 ToyPos(GameObject toy)
+        {
+            Vector3 targetPos = new Vector3();
+            targetPos = toy.transform.position;
+            targetPos.y = transform.position.y;
+            return targetPos;
+        }
+
+        #endregion
+
+    #region Interactions
+        private void HeadScratch()
+        {
+
+        }
+        private void BodyScratch()
+        {
+
+
+        }
+    private void StopHeadScratch()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void StopBodyScratch()
+    {
+        throw new NotImplementedException();
+    }
+    private void DogCall(Vector3 playerPos)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DogSit(Vector3 playerPos)
+    {
+        throw new NotImplementedException();
+    }
+    private void DogPickedUp()
+        {
+            pickedUp = true;
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+        private void DogDropped()
+        {
+            pickedUp = false;
+        }
+
+        
+
+        #endregion
+
+    #region Movement nad Positioning
+    private int DistanceToPoint(Vector3 target)
+    {
+        int difference = 0;
+        Vector3 differenceVector = new Vector3(0, 0, 0);
+        if (target.x < transform.position.x)
+        {
+            differenceVector.x = transform.position.x - target.x;
+        }
+        else
+        {
+            differenceVector.x = target.x - transform.position.x;
+        }
+        if (target.z < transform.position.z)
+        {
+            differenceVector.z = transform.position.z - target.z;
+        }
+        else
+        {
+            differenceVector.z = target.z - transform.position.z;
+        }
+        difference = (int)differenceVector.x + (int)differenceVector.z;
+        print(difference);
+        return difference;
+    }
+
+        public void CloseToSomething(Seekable seek)
+    {
+        switch (seek)
+        {
+            case Seekable.Player:
+                followPlayer = false;
+                closeToPlayer = true;
+             break;
+            case Seekable.Toy:
+                toySeen = false;
+                PickUpToy();
+                break;
+        }
+    }
+
+    void Wandering()
+    {
+        Lerping(gridScript.GetRandomNode());
+        animator.SetFloat("Move", .6f);
+    }
+
+    bool OutOfBounds()
+    {
+        if ((transform.position.x >= -3) || (transform.position.x <= -15))
+            return true;
+        else if ((transform.position.z >= 4) || (transform.position.z <= -12))
+            return true;
+        else
+            return false;
+
+    }
+
+    #endregion
+}
---
+++ Happy Hounds VR/Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public List<Vector3> nodeCoords;
+    public List<Vector3> blockedNodes;
+    public Node[] nodes;
+
+    [SerializeField]
+    protected int lengthX = 28;
+    [SerializeField]
+    protected int lengthZ = 28;
+    [SerializeField]
+    protected Vector3 gridCorner;
+
+    public float dogYPos;
+
+    public bool showNodes;
+
+    private void Start()
+    {
+        InstantiateGrid();
+        
+        if (showNodes)
+            ShowNodes();
+    }
+
+    // lengthX = 3
+    // lengthZ = 5
+    // 0,  1,  2,  3,  4,
+    // 5,  6,  7,  8,  9,
+    // 10, 11 12, 13, 14,
+
+    // 0,  1,  2,  3,  4, 5,  6,  7,  8,  9, 10, 11 12, 13, 14,
+
+    /// <summary>
+    /// converts world position to closest node
+    /// </summary>
+    /// <param name="coord"></param>
+    /// <returns></returns>
+    public Node coordToNode(Vector3 coord)
+    {
+        coord.x = Mathf.Round(coord.x);
+        coord.y = dogYPos;
+        coord.z = Mathf.Round(coord.z);
+
+        for (int i=0; i<nodes.Length; i++)
+        {
+            if (nodes[i].coord == coord)
+            {
+                return nodes[i];
+            }
+        }
+        return nodes[0];
+    }
+
+    /// <summary>
+    /// gets traversable area in vector 3
+    /// </summary>
+    void InstantiateGrid()
+    {
+        for (int i = 0; i < lengthX; i++)
+        {
+            for(int j= 0; j < lengthZ; j++)
+            {
+                nodeCoords.Add(new Vector3(gridCorner.x - i, dogYPos, gridCorner.z - j));
+            }
+        }
+
+        foreach (Vector3 v in blockedNodes)
+            nodeCoords.Remove(v);
+
+        InstantiateNodes();
+    }
+
+    /// <summary>
+    /// translates vectors 3 into nodes
+    /// </summary>
+    void InstantiateNodes()
+    {
+        nodes = new Node[nodeCoords.Count];
+        for(int i=0; i < nodeCoords.Count; i++)
+        {
+            Node node = new Node();
+            node.coord = nodeCoords[i];
+            nodes[i] = node;
+        }
+
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            Node node = nodes[i];
+            for (int x = 0; x < 3; x++)
+            {
+                for (int z = 0; z < 3; z++)
+                {
+                    Vector3 neighbourPos = node.coord;
+                    neighbourPos.x +=  x - 1;
+                    neighbourPos.z +=  z - 1;
+
+                    Node neighbour = coordToNode(neighbourPos);
+                    if(node != null)
+                    {
+                        node.connectedNodes.Add(neighbour);
+                    }
+                }
+            }
+        }
+    }
+
+
+    
+    /// <summary>
+    /// used for visualisation of nodes for debugging
+    /// </summary>
+    void ShowNodes()
+    {
+        foreach(Vector3 v in nodeCoords)
+        {
+            float sphereScale = .5f;
+            GameObject nodeObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            nodeObject.transform.localScale = new Vector3(sphereScale, sphereScale, sphereScale);
+            nodeObject.transform.position = v;
+        }
+    }
+
+    /// <summary>
+    /// returns a random node
+    /// </summary>
+    /// <returns></returns>
+    public Node GetRandomNode()
+    {
+        int i = Random.Range(0, nodes.Length);
+        print("fucking last point: " + nodes[i].coord);
+        return nodes[i];
+    }
+}
